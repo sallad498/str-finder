ac41ce67142ceeec0723610ced908dc109751d31
sallad498 <jared.dallas1@gmail.com>
2026-05-14 20:26:11 -0400

    Fix API mapping and eslint warning

--- src/App.js
+++ src/App.js
@@ -350,7 +350,8 @@ export default function App() {
     setFetchStatus("done");
   }
 
-  useEffect(() => { fetchAll(false); }, [fetchAll]); // eslint-disable-line react-hooks/exhaustive-deps
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => { fetchAll(false); }, []);
 
   const filtered = useMemo(() => allListings.filter(l =>
     (selCounty === "All" || l.county === selCounty) &&
